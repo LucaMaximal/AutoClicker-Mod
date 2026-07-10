1266c06a05ecba4d3816f1eebaaa70e1a940f3b9
LucaMaximal <luca.golbs@icloud.com>
2026-07-10 23:09:47 +0200

    Match official Fabric 26.2 Gradle template

--- src/main/java/io/github/itzispyder/autoclicker/AutoClicker.java
+++ src/main/java/io/github/itzispyder/autoclicker/AutoClicker.java
@@ -1,23 +1,22 @@
 package io.github.itzispyder.autoclicker;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import io.github.itzispyder.autoclicker.ui.Config;
 import io.github.itzispyder.autoclicker.ui.Events;
 import io.github.itzispyder.improperui.ImproperUIAPI;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
 public class AutoClicker implements ModInitializer, Global {
 
-    public static final KeyBinding BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+    public static final KeyMapping BIND = KeyBindingHelper.registerKeyBinding(new KeyMapping(
             "binds.autoclicker.menu",
-            InputUtil.Type.KEYSYM,
+            InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_0,
-            KeyBinding.Category.create(Identifier.of("binds.autoclicker"))
+            "binds.autoclicker"
     ));
 
     @Override
@@ -33,7 +32,7 @@ public class AutoClicker implements ModInitializer, Global {
             Events.onTick();
         });
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (BIND.wasPressed()) {
+            while (BIND.consumeClick()) {
                 ImproperUIAPI.parseAndRunFile(modId, "screen.ui");
             }
         });
